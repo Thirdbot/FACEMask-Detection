d807f910d0e2eba11562d9c0b80814c559a689e3
WarinCode <warinsaipanya471@outlook.co.th>
2025-05-19 00:18:26 +0700

    Remove unused spline viewer and clean up Home component

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -8,25 +8,16 @@ import Title from "../ui/Title";
 const Home = () => {
   return (
     <AppContainer>
-      <div className="fixed top-0 left-0 w-full h-full -z-10">
-        <spline-viewer
-          url="https://prod.spline.design/m9pRL88zqUsjApin/scene.splinecode"
-          style={{ width: "100%", height: "100%" }}
-        ></spline-viewer>
-      </div>
       <Sidebar />
       <PageContent>
-        <div className="text-red-500">
-          <Title text="หน้าหลัก" />
-        </div>
+        <Title text="หน้าหลัก" />
         <img
           src="/assets/icons/logo.png"
           alt="ku-logo"
           className="size-60 shadow-xl"
         />
-      <div className="w-full mt-10 overflow-hidden leading-8 tracking-wide text-red-500">
-        <label htmlFor="">
-          <span className="font-bold ">KU FaceMask </span>
+        <p className="w-full mt-10 overflow-hidden leading-8 tracking-wide">
+          <span className="font-bold">KU FaceMask </span>
           เป็นเว็บไซต์ที่ถูกพัฒนาและออกแบบโดยนิสิตนักศึกษาชั้นปีที่ 2 ของ
           มหาวิทยาลัยเกษตรศาสตร์ วิทยาเขตศรีราชา นิสิต คณะวิทยาศาสตร์ สาขา
           วิทยาการคอมพิวเตอร์ ภาคพิเศษ เว็บไซต์นี้เป็นส่วนหนึ่งของโปรเจควิชา
@@ -51,8 +42,7 @@ const Home = () => {
             หน้ารายละเอียดโปรเจค
           </NavLink>
           </Tooltip>
-        </label>
-        </div> 
+        </p>
       </PageContent>
     </AppContainer>
   );
