6c6e25ba381eb679649adc36717ce507461510aa
WarinCode <warinsaipanya471@outlook.co.th>
2025-05-26 13:03:01 +0700

    Update index.py

--- models/index.py
+++ models/index.py
@@ -99,6 +99,4 @@ model.fit(datagen.flow(x_train, y_train, batch_size=16), epochs=14, validation_d
 
 model.evaluate(x_test, y_test)
 
-model.save('Full_modelRGB.h5')
-
-model = load_model("Full_modelRGB.h5")
+model.save('model.h5')
