5f89b5952ab3575365f6f8252b6eb6b94ec4b301
Third <astroll656@gmail.com>
2025-05-30 12:11:57 +0700

    requirement and runner

--- Setup.py
+++ Setup.py
@@ -2,21 +2,26 @@ from Train import Trainer
 import kagglehub
 from pathlib import Path
 
-Home_dir = Path(__file__).parent.absolute()
-# Download latest version "pranavsingaraju/facemask-detection-dataset-20000-images"
-dataset_path = Home_dir / "cleaned_dataset" / "data"
+def run_train():
+    Home_dir = Path(__file__).parent.absolute()
+    # Download latest version "pranavsingaraju/facemask-detection-dataset-20000-images"
+    dataset_path = Home_dir / "cleaned_dataset" / "data"
 
-trainer = Trainer(path=dataset_path)
+    trainer = Trainer(path=dataset_path)
 
-###model list 
-model_list = ["DeepLearning","DecisionClass","KNNClass","RFC"]
-model_name = "DeepLearning"
+    ###model list 
+    model_list = ["DeepLearning","DecisionClass","KNNClass","RFC"]
+    model_name = "DeepLearning"
 
-#using with single train
-trainer.create_model(model_name)
-save_best = trainer.train(model_name)
-#using without declare creating model
-# save_best = trainer.train_all()
+    #using with single train
+    trainer.create_model(model_name)
+    save_best = trainer.train(model_name)
+    #using without declare creating model
+    # save_best = trainer.train_all()
 
-print("save_best",save_best)
+    print("save_best",save_best)
+
+if __name__ == "__main__":
+    run_train()
+
 
